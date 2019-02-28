fffc182c3e6f2d8f8a371bd81e7ceec77369c3b9
kuimo75 <kuimo75@gmail.com>
2019-02-28 21:37:46 +0900

    Release v1.0 (+Bug Fix)

--- RootFinder/PossibleRoots.cs
+++ RootFinder/PossibleRoots.cs
@@ -1,8 +1,6 @@
 ﻿using org.mariuszgromada.math.mxparser;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace RootFinder
 {
--- RootFinder/VisualFunction.cs
+++ RootFinder/VisualFunction.cs
@@ -5,8 +5,6 @@ using OpenTK.Graphics.OpenGL;
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
 using RootFinder.Properties;
